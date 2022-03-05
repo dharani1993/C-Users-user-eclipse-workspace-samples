ea817047b6941b3c9682c9ac8958fee50b14a1de
dharani1993 <dharanimeenana@gmail.com>
2022-03-05 18:00:47 +0530

    RET123

---
+++ src/org/sample/task.java
@@ -0,0 +1,32 @@
+package org.sample;
+
+public class task {
+
+	public static void main(String[] args) {
+		for (int i = 1; i <=7; i++) {
+			
+			for (int j =9 - 8; j>=8 ; j--) {
+				System.out.print(" ");
+				
+			}
+			for (int k = 0; k <=i; k++) {
+				
+			System.out.print("*");
+			}
+			System.out.println();
+			
+			
+			
+		}
+		
+		
+		
+		
+	}
+	
+	
+	
+	
+	
+	
+}
